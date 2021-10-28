7d5e8ee4b23a68404b38c4b7fd3a39b2b9809e9f
Aman-US-Punjabi <aman_sehdev@live.com>
2021-10-27 20:15:31 -0700

    Added Entities in Core and CatalogDbContext
    Added Product and ProductImage entities, added them to catalogDbContext and added migration, updated database

---
+++ OnlineStore.Core/Entities/BaseEntities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace OnlineStore.Core.Entities.BaseEntities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedUtc { get; set; }
+        public DateTime? LastModifiedUtc { get; set; }
+
+        public BaseEntity()
+        {
+        }
+    }
+}
--- OnlineStore.Infrastructure/Data/Migrations/20211028030951_ProductAndProductImageTables.Designer.cs
+++ OnlineStore.Infrastructure/Data/Migrations/20211028030951_ProductAndProductImageTables.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OnlineStore.Infrastructure.Data;
 
 namespace OnlineStore.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(CatalogDbContext))]
-    partial class CatalogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211028030951_ProductAndProductImageTables")]
+    partial class ProductAndProductImageTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
