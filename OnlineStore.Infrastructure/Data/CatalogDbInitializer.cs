3eb87e874e2fd2b9b9b121950683b774716f1f1d
Aman-US-Punjabi <aman_sehdev@live.com>
2021-10-31 16:06:26 -0700

    Initialize Database and seed data while 1st time running app
    Seed Admin User, Roles and Products; intialize Database while running app for 1st time.

---
+++ OnlineStore.Infrastructure/Data/CatalogDbInitializer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using OnlineStore.Core.Entities.Catalog;
+
+namespace OnlineStore.Infrastructure.Data
+{
+    public static class CatalogDbInitializer
+    {
+
+        public static void Initialize(CatalogDbContext catalogDbContext)
+        {
+            try
+            {
+                if (catalogDbContext.Database.GetPendingMigrations().Count() > 0)
+                {
+                    catalogDbContext.Database.Migrate();
+                }
+            }
+            catch (Exception ex)
+            {
+                // TODO Log Error
+                //var logger = services.GetRequiredService<ILogger<Program>>();
+                //logger.LogError(ex, "An error occurred migrating the CatalogDB.");
+                Console.WriteLine("An error occurred migrating the CatalogDB.");
+            }
+            SeedProducts(catalogDbContext);
+        }
+
+        private static void SeedProducts(CatalogDbContext catalogDbContext)
+        {
+            // Look for any Products.
+            if (catalogDbContext.Products.Any())
+            {
+                return; // Db has been seeded
+            }
+
+            var products = new Product[]
+           {
+                new Product()
+                {
+                    Name = "iPhone 12",
+                    Price = 1150,
+                    Quantity = 12,
+                    Description = "Apple iPhone 13, 3 cameras, 100 GB storage, includes a box with charging cable."
+                },
+                new Product()
+                {
+                    Name = "iPhone 12 pro",
+                    Price = 1250,
+                    Quantity = 2,
+                    Description = "Apple iPhone 12 pro, 3 cameras, 100 GB storage, includes a box with charging cable."
+                },
+                new Product()
+                {
+                    Name = "iPhone 13",
+                    Price = 1350,
+                    Quantity = 12,
+                    Description = "Apple iPhone 13, 3 cameras, 100 GB storage, includes a box with charging cable."
+                },
+                new Product()
+                {
+                    Name = "iPhone 13 pro",
+                    Price = 1450,
+                    Quantity = 12,
+                    Description = "Apple iPhone 13 pro, 3 cameras, 100 GB storage, includes a box with charging cable."
+                },
+           };
+
+            catalogDbContext.Products.AddRange(products);
+            catalogDbContext.SaveChanges();
+        }
+    }
+}
---
+++ OnlineStore.Infrastructure/Identity/AppIdentityDbContextInitializer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using OnlineStore.Core;
+
+namespace OnlineStore.Infrastructure.Identity
+{
+    public static class AppIdentityDbContextInitializer
+    {
+        public static void SeedAsync(AppIdentityDbContext appIdentityDbContext, UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            try
+            {
+                if (appIdentityDbContext.Database.GetPendingMigrations().Count() > 0)
+                {
+                    appIdentityDbContext.Database.Migrate();
+                }
+            }
+            catch (Exception ex)
+            {
+                //var logger = services.GetRequiredService<ILogger<Program>>();
+                //logger.LogError(ex, "An error occurred migrating the AppIdentityDB.");
+                // TODO Log error
+                Console.WriteLine("An error occurred migrating the AppIdentityDB.");
+            }
+
+            SeedRoles(roleManager);
+            SeedUsers(userManager);
+        }
+
+        private static void SeedUsers(UserManager<ApplicationUser> userManager)
+        {
+            if (!userManager.Users.Any())
+            {
+                var adminUser = new ApplicationUser
+                {
+                    Email = SD.Admin_Email,
+                    UserName = SD.Admin_Email,
+                    FirstName = "Aman",
+                    LastName = "Singh",
+                    EmailConfirmed = true
+                };
+
+                IdentityResult result = userManager.CreateAsync(adminUser, SD.Admin_Password).Result;
+
+                if (result.Succeeded)
+                {
+                    var admin = userManager.FindByNameAsync(SD.Admin_Email).Result;
+                    userManager.AddToRolesAsync(admin, new[] { SD.Role_Admin }).Wait();
+                }
+            }
+        }
+
+        // TODO Currently when we add user and add admin role while creating User, If statement
+        // fails because there is already UserRole admin in the roles. So, no roles are being created
+        private static void SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            if (!roleManager.Roles.Any())
+            {
+                var roles = new List<IdentityRole>
+                {
+                    new IdentityRole{ Name = SD.Role_Admin },
+                    new IdentityRole{ Name = SD.Role_Customer },
+                    new IdentityRole{ Name = SD.Role_Employee }
+                };
+
+                foreach (var role in roles)
+                {
+                    roleManager.CreateAsync(role).Wait();
+                }
+            }
+        }
+    }
+}
