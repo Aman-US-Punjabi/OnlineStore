addc4bfbfc7692996f01a3b01a7e68f3da981332
Aman-US-Punjabi <aman_sehdev@live.com>
2021-11-04 12:37:06 -0700

    Add CoreXUnitTest Project and Add tests for DateInFutureAttribute

---
+++ OnlineStore.Core.Tests/DateInFutureAttributeTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using OnlineStore.Core.Entities.EntityValidations;
+using Xunit;
+
+namespace OnlineStore.Core.Tests
+{
+    public class DateInFutureAttributeTests
+    {
+        public DateInFutureAttributeTests()
+        {
+        }
+
+        [Theory]
+        [InlineData(100, true)]
+        [InlineData(-100, false)]
+        public void DateValidator_InputexpectedDateRange_DateValidity(int addTime, bool expectedResult)
+        {
+            DateInFutureAttribute dateInFutureAttribute = new(() => DateTime.Now);
+
+            var result = dateInFutureAttribute.IsValid(DateTime.Now.AddSeconds(addTime));
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void DateValidator_NotValidDate_ReturnErrorMessage()
+        {
+            var result = new DateInFutureAttribute();
+
+            Assert.Equal("Date must be in the future", result.ErrorMessage);
+        }
+    }
+}
